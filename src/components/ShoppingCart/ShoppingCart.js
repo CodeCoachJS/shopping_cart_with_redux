321ee473e4c58334537a7e963019a20f6b89c459
Brian Jenney <brianjenney83@gmail.com>
2023-05-06 09:48:47 -0700

    remove eslint config breaking stuff

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,42 +1,42 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleCart } from "../../ducks/shopping.ducks";
-import styles from "./ShoppingCart.module.css";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleCart } from '../../ducks/shopping.ducks';
+import styles from './ShoppingCart.module.css';
 
 const ShoppingCart = () => {
-  const dispatch = useDispatch();
-  const { cart } = useSelector((state) => state);
+	const dispatch = useDispatch();
+	const { cart } = useSelector((state) => state);
 
-  if (!cart.isOpen) {
-    return <></>;
-  }
+	if (!cart.isOpen) {
+		return <></>;
+	}
 
-  const { items = [] } = cart;
-  const total = items.reduce((acc, item) => item.price + acc, 0);
+	const { items = [] } = cart;
+	const total = items.reduce((acc, item) => item.price + acc, 0);
 
-  return (
-    <div className={styles.shoppingCart}>
-      <div
-        className={styles.closeIcon}
-        onClick={() => dispatch(toggleCart(false))}
-      >
-        X
-      </div>
-      <h2>Items</h2>
-      {items.map((item) => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <img width="100" alt={item.title} src={item.image} />
-          <div>
-            <small>${item.price}</small>
-          </div>
+	return (
+		<div className={styles.shoppingCart}>
+			<div
+				className={styles.closeIcon}
+				onClick={() => dispatch(toggleCart(false))}
+			>
+				X
+			</div>
+			<h2>Items</h2>
+			{items.map((item) => (
+				<div key={item.id}>
+					<p>{item.title}</p>
+					<img width='100' alt={item.title} src={item.image} />
+					<div>
+						<small>${item.price}</small>
+					</div>
 
-          <button>Remove</button>
-        </div>
-      ))}
-      <h2>Total: ${total.toFixed(2)}</h2>
-    </div>
-  );
+					<button>Remove</button>
+				</div>
+			))}
+			<h2>Total: ${total.toFixed(2)}</h2>
+		</div>
+	);
 };
 
 export default ShoppingCart;
